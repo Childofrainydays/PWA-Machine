72e806bf58162e3e7b1d175ccf1f438e140d5714
Childofrainydays <107083153+Childofrainydays@users.noreply.github.com>
2023-08-06 14:45:46 -0600

    install.js handlers

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,27 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// An event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    window.deferredPrompt = event;
+    // removes the `hidden` attribute from the install button
+    butInstall.removeAttribute('hidden');
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    // Get the deferred prompt
+    const installEvent = window.deferredPrompt;
+    // If there is no deferred prompt, exit the function
+    if (!installEvent) {
+        return;
+    }
+    installEvent.prompt();
+
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+
+});
